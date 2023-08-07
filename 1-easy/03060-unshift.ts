11657efc6573d601d208d20a2a94c03c9a66f84f
Dominic Carrington <carringtondominic@gmail.com>
2023-08-07 23:41:41 +0100

    [easy/unshift] Completed

---
+++ 1-easy/03060-unshift.ts
@@ -0,0 +1,16 @@
+/* === PROBLEM === */
+type Unshift<T extends any[], U> = [U, ...T];
+
+/* 
+=== EXPLANATION ===
+Using array spread function, this is quite simple.
+*/
+
+/* === TEST CASES === */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<["1", 2, "3"], boolean>, [boolean, "1", 2, "3"]>>
+];
