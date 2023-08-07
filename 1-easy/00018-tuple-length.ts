053c4694685c95608691b6a7fbb8b8eac9fb09ff
Dominic Carrington <carringtondominic@gmail.com>
2023-08-07 18:45:48 +0100

    [easy/tuple-length] Complete

---
+++ 1-easy/00018-tuple-length.ts
@@ -0,0 +1,23 @@
+/* === PROBLEM === */
+type Length<TArray extends readonly any[]> = TArray["length"];
+
+/* === TEST CASES === */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tesla = ["tesla", "model 3", "model X", "model Y"] as const;
+const spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT",
+] as const;
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<"hello world">
+];
