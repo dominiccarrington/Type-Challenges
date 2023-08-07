a73a7cd48f5b04e7c05853f2339f92b501cf8463
Dominic Carrington <carringtondominic@gmail.com>
2023-08-07 18:57:56 +0100

    [easy/exclude] Complete

---
+++ 1-easy/00043-exclude.ts
@@ -0,0 +1,15 @@
+/* === PROBLEM === */
+type MyExclude<TOriginal, TRemove> = TOriginal extends TRemove
+  ? never
+  : TOriginal;
+
+/* === TEST CASES === */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
